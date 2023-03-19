5cce4ebd777c19a415f80133ae8ceb57c49c525f
gnehs <jayuiop@gmail.com>
2023-03-19 18:02:36 +0800

    Initial commit
    fixed token

---
+++ README.md
@@ -0,0 +1,36 @@
+# GitLab Issue Telegram Bot
+
+這個機器人將定期從 GitLab 抓取 issue 動態，並將更新發送到指定的 Telegram 頻道。
+
+GPT-4-0314 寫的，有問題不要找我，自己去找他修，就這樣。
+
+## 功能
+
+- 每分鐘獲取 GitLab issue 動態
+- 將新的 issue 更新發送到指定的 Telegram 頻道
+- 記住已經發送過的 issue 以避免重複發送
+- 使用 Markdown 格式美化訊息內容
+
+## 安裝與使用
+
+1. 確保您已經安裝了 [Node.js](https://nodejs.org/)。
+2. 使用以下命令安裝依賴套件：
+   ```
+   npm install telegraf node-fetch jsdom
+   ```
+3. 為機器人設置環境變數（使用實際的值替換以下示例）：
+   - `TELEGRAM_BOT_TOKEN`: 您的 Telegram Bot Token
+   - `TELEGRAM_CHANNEL_ID`: 要發送訊息的 Telegram 頻道 ID（例如：-1001962261708）
+   - `GITLAB_FEED_URL`: GitLab RSS 連結（例如：https://gitlab.com/HacksInTaiwan/2023/board.atom?feed_token=xxxxxx）
+4. 在與 `gitlab-telegram-bot.js` 相同的目錄中創建一個名為 `sent_issues.json` 的文件，並將其初始內容設置為 `[]`。
+5. 在命令行中設置環境變數並運行此文件以啟動機器人（根據您的操作系統選擇相應的命令）：
+   - 對於 Unix/Linux/macOS：
+     ```
+     TELEGRAM_BOT_TOKEN=your_bot_token TELEGRAM_CHANNEL_ID=-1001962261708 GITLAB_FEED_URL=https://gitlab.com/HacksInTaiwan/2023/board.atom?feed_token=xxxxxx node gitlab-telegram-bot.js
+     ```
+   - 對於 Windows：
+     ```
+     set TELEGRAM_BOT_TOKEN=your_bot_token && set TELEGRAM_CHANNEL_ID=-1001962261708 && set GITLAB_FEED_URL=https://gitlab.com/HacksInTaiwan/2023/board.atom?feed_token=xxxxxx && node gitlab-telegram-bot.js
+     ```
+
+現在機器人將每分鐘獲取 GitLab issue 動態，並將它們發送到指定的 Telegram 頻道。
---
+++ index.js
@@ -0,0 +1,79 @@
+import { Telegraf } from 'telegraf';
+import fetch from 'node-fetch';
+import { JSDOM } from 'jsdom';
+import fs from 'fs';
+
+// 設置你的 Telegram Bot Token
+const bot = new Telegraf(process.env.TELEGRAM_BOT_TOKEN);
+
+const CHANNEL_ID = process.env.TELEGRAM_CHANNEL_ID;
+const GITLAB_FEED_URL = process.env.GITLAB_FEED_URL;
+const SENT_ISSUES_FILE = 'sent_issues.json';
+
+// 從 JSON 文件中讀取已發送的 issue ID
+function loadSentIssueIds() {
+  try {
+    const data = fs.readFileSync(SENT_ISSUES_FILE, 'utf8');
+    const issueIds = JSON.parse(data);
+    return new Set(issueIds);
+  } catch (error) {
+    // 返回一個空的 Set
+    return new Set();
+  }
+}
+
+// 將新的 issue ID 添加到 JSON 文件中
+function saveSentIssueId(issueId) {
+  sentIssueIds.add(issueId);
+  const data = JSON.stringify(Array.from(sentIssueIds));
+  fs.writeFileSync(SENT_ISSUES_FILE, data);
+}
+// 獲取 GitLab issue 動態
+async function fetchGitLabIssues() {
+  const response = await fetch(GITLAB_FEED_URL);
+  const text = await response.text();
+  const dom = new JSDOM(text);
+  const document = dom.window.document;
+  const entries = document.querySelectorAll('entry');
+
+  let issues = [];
+
+  for (let entry of entries) {
+    const linkElement = entry.querySelector('link');
+    const link = linkElement ? linkElement.getAttribute('href') : '';
+
+    issues.push({
+      id: entry.querySelector('id').textContent,
+      title: entry.querySelector('title').textContent,
+      updated: entry.querySelector('updated').textContent,
+      summary: entry.querySelector('summary').textContent,
+      link,
+    });
+  }
+
+  return issues;
+}
+
+// 發送 GitLab issue 至 Telegram 頻道
+async function sendGitLabIssuesToTelegram() {
+  const issues = await fetchGitLabIssues();
+
+  for (let issue of issues) {
+    // 檢查 issue 是否已經發送過
+    if (!sentIssueIds.has(issue.id)) {
+      const message = `🆕 Issue 更新！\n\n[Title: ${issue.title}](${issue.link})\nUpdated: ${issue.updated}\nSummary: ${issue.summary}`;
+      await bot.telegram.sendMessage(CHANNEL_ID, message, { parse_mode: 'MarkdownV2' });
+      // 將 issue ID 添加到已發送集合中，並更新 JSON 文件
+      saveSentIssueId(issue.id);
+    }
+  }
+}
+
+// 讀取已發送 issue ID
+let sentIssueIds = loadSentIssueIds();
+
+// 每分鐘獲取 GitLab issue 動態並發送至 Telegram 頻道
+setInterval(sendGitLabIssuesToTelegram, 60 * 1000);
+
+bot.launch();
+sendGitLabIssuesToTelegram()
